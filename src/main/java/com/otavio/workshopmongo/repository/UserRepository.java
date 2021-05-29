70a2088fd2c8079380abc8130643fa274f762f48
Otavio Ramos <otavio.santana@portaldedocumentos.com.br>
2021-05-29 20:15:34 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/otavio/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.otavio.workshopmongo.repository;
+
+import com.otavio.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
