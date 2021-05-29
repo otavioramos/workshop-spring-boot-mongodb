be9a05432cd7debb9b7bc3e101b423896d978b7e
Otavio Ramos <otavio.santana@portaldedocumentos.com.br>
2021-05-29 20:30:48 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/otavio/workshopmongo/domain/User.java
+++ src/main/java/com/otavio/workshopmongo/domain/User.java
@@ -9,7 +9,8 @@ import java.util.Objects;
 @Document(collection = "user")
 public class User implements Serializable {
 
-    @Id
+	private static final long serialVersionUID = 1L;
+	@Id
     private String id;
     private String name;
     private String email;
--- src/main/java/com/otavio/workshopmongo/resources/UserResource.java
+++ src/main/java/com/otavio/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.otavio.workshopmongo.resources;
 
-import com.otavio.workshopmongo.domain.User;
-import com.otavio.workshopmongo.services.UserService;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.otavio.workshopmongo.domain.User;
+import com.otavio.workshopmongo.dto.UserDTO;
+import com.otavio.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,8 +21,9 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
