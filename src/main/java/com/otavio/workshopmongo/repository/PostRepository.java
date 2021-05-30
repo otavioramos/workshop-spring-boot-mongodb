cd7ed1585c6d44cb2f39c1b35d140baeabe0ee90
Otavio Ramos <otavio.omak@gmail.com>
2021-05-30 15:06:22 -0300

    Consulta simples com @Query

--- src/main/java/com/otavio/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/otavio/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.otavio.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.otavio.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
